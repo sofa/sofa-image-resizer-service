7d559b7b71153065f30767a1e9b5419a5d7147a6
Pascal Precht <pascal.precht@googlemail.com>
2014-04-16 13:16:41 +0200

    wip

--- src/sofa.imageResizerService.js
+++ src/sofa.imageResizerService.js
@@ -2,84 +2,84 @@
 /* global sofa */
 
 sofa.define('sofa.ImageResizerService', function (configService) {
-        var RESIZER_ENDPOINT = configService.get('imageResizerEndpoint');
-        var RESIZER_ENABLED = configService.get('imageResizerEnabled');
+    var RESIZER_ENDPOINT = configService.get('imageResizerEndpoint');
+    var RESIZER_ENABLED = configService.get('imageResizerEnabled');
 
-        //http://stackoverflow.com/questions/1714786/querystring-encoding-of-a-javascript-object
-        function objectToQueryString(obj) {
-            var str = [];
-            for (var p in obj) {
-                if (obj.hasOwnProperty(p)) {
-                    str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
-                }
+    //http://stackoverflow.com/questions/1714786/querystring-encoding-of-a-javascript-object
+    function objectToQueryString(obj) {
+        var str = [];
+        for (var p in obj) {
+            if (obj.hasOwnProperty(p)) {
+                str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
             }
-            return str.join('&');
+        }
+        return str.join('&');
+    }
+
+    // http://www.developerdrive.com/2013/08/turning-the-querystring-into-a-json-object-using-javascript/
+    function queryStringToObject(queryString) {
+        var pairs = queryString.split('&');
+        var result = {};
+        pairs.forEach(function (pair) {
+            pair = pair.split('=');
+            result[pair[0]] = decodeURIComponent(pair[1] || '');
+        });
+        return JSON.parse(JSON.stringify(result));
+    }
+
+    var resizeUrlCache = {},
+        self = {};
+
+    self.resize = function (imageUrl, args) {
+        if (!RESIZER_ENABLED) {
+            return (imageUrl);
         }
 
-        // http://www.developerdrive.com/2013/08/turning-the-querystring-into-a-json-object-using-javascript/
-        function queryStringToObject(queryString) {
-            var pairs = queryString.split('&');
-            var result = {};
-            pairs.forEach(function (pair) {
-                pair = pair.split('=');
-                result[pair[0]] = decodeURIComponent(pair[1] || '');
-            });
-            return JSON.parse(JSON.stringify(result));
+        var cacheKey;
+        if (sofa.Util.isString(args)) {
+            cacheKey = imageUrl + args;
+        } else {
+            cacheKey = imageUrl + objectToQueryString(args);
+        }
+        if (resizeUrlCache[cacheKey]) {
+            return resizeUrlCache[cacheKey];
         }
 
-        var resizeUrlCache = {},
-            self = {};
+        // no cache hit, decode string argument to object if required
+        if (sofa.Util.isString(args)) {
+            args = queryStringToObject(args);
+        }
+        if (!args || !args.hasOwnProperty('maxwidth') || !args.hasOwnProperty('maxheight')) {
+            throw new Error('maxwidth and maxheight are required parameters');
+        }
 
-        self.resize = function (imageUrl, args) {
-            if (!RESIZER_ENABLED) {
-                return (imageUrl);
-            }
+        // Add protocol if not specified in URL
+        if (/^\/\//.test(imageUrl)) {
+            imageUrl = window.location.protocol + imageUrl;
+        }
 
-            var cacheKey;
-            if (sofa.Util.isString(args)) {
-                cacheKey = imageUrl + args;
-            } else {
-                cacheKey = imageUrl + objectToQueryString(args);
-            }
-            if (resizeUrlCache[cacheKey]) {
-                return resizeUrlCache[cacheKey];
-            }
+        var defaults = {
+            cmd: 'resize',
+            url: imageUrl,
+            quality: 100,
+            devicePixelRatio: window.devicePixelRatio
+        },
+            fullArgs = sofa.Util.extend(args, defaults),
+            imageExt = imageUrl.substring(imageUrl.lastIndexOf('.') + 1);
 
-            // no cache hit, decode string argument to object if required
-            if (sofa.Util.isString(args)) {
-                args = queryStringToObject(args);
-            }
-            if (!args || !args.hasOwnProperty('maxwidth') || !args.hasOwnProperty('maxheight')) {
-                throw new Error('maxwidth and maxheight are required parameters');
-            }
+        // In case of retina display, need to fetch larger image
+        if (args.devicePixelRatio > 1) {
+            fullArgs.maxwidth *= args.devicePixelRatio;
+            fullArgs.maxheight *= args.devicePixelRatio;
+        }
 
-            // Add protocol if not specified in URL
-            if (/^\/\//.test(imageUrl)) {
-                imageUrl = window.location.protocol + imageUrl;
-            }
+        var encodedCall = window.btoa(objectToQueryString(fullArgs)).replace('/', '_') + '.' + imageExt,
+            resizedImageUrl = RESIZER_ENDPOINT + encodedCall;
 
-            var defaults = {
-                cmd: 'resize',
-                url: imageUrl,
-                quality: 100,
-                devicePixelRatio: window.devicePixelRatio
-            },
-                fullArgs = sofa.Util.extend(args, defaults),
-                imageExt = imageUrl.substring(imageUrl.lastIndexOf('.') + 1);
+        resizeUrlCache[cacheKey] = resizedImageUrl;
 
-            // In case of retina display, need to fetch larger image
-            if (args.devicePixelRatio > 1) {
-                fullArgs.maxwidth *= args.devicePixelRatio;
-                fullArgs.maxheight *= args.devicePixelRatio;
-            }
+        return resizedImageUrl;
+    };
 
-            var encodedCall = window.btoa(objectToQueryString(fullArgs)).replace('/', '_') + '.' + imageExt,
-                resizedImageUrl = RESIZER_ENDPOINT + encodedCall;
-
-            resizeUrlCache[cacheKey] = resizedImageUrl;
-
-            return resizedImageUrl;
-        };
-
-        return self;
-    });
+    return self;
+});
