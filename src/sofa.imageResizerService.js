297f42fa7c9731405c6e43f0c6fae7f9e755157f
Ingo Blenke <info@ingoblenke.de>
2014-06-18 15:17:11 +0200

    fix(sofa-image-resizer-service): brought back the base64encode polyfill

--- src/sofa.imageResizerService.js
+++ src/sofa.imageResizerService.js
@@ -1,7 +1,40 @@
 'use strict';
-/* global sofa */
+/* global sofa, base64Encode */
 
 sofa.define('sofa.ImageResizerService', function (configService, $window) {
+
+    // http://phpjs.org/functions/base64_encode/
+    /* jshint ignore:start */
+    var base64Encode = function (data) {
+        if (typeof $window.btoa === 'function') {
+            return $window.btoa(data);
+        }
+        var b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+        var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+            ac = 0,
+            enc = '',
+            tmpArr = [];
+        if (!data) {
+            return data;
+        }
+        do { // pack three octets into four hexets
+            o1 = data.charCodeAt(i++);
+            o2 = data.charCodeAt(i++);
+            o3 = data.charCodeAt(i++);
+            bits = o1 << 16 | o2 << 8 | o3;
+            h1 = bits >> 18 & 0x3f;
+            h2 = bits >> 12 & 0x3f;
+            h3 = bits >> 6 & 0x3f;
+            h4 = bits & 0x3f;
+            // use hexets to index into b64, and append result to encoded string
+            tmpArr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
+        } while (i < data.length);
+        enc = tmpArr.join('');
+        var r = data.length % 3;
+        return (r ? enc.slice(0, r - 3) : enc) + '==='.slice(r || 3);
+    };
+    /* jshint ignore:end */
+
     //http://stackoverflow.com/questions/1714786/querystring-encoding-of-a-javascript-object
     function objectToQueryString(obj) {
         var str = [];
@@ -70,7 +103,7 @@ sofa.define('sofa.ImageResizerService', function (configService, $window) {
             fullArgs.maxheight *= args.devicePixelRatio;
         }
 
-        var encodedCall = $window.btoa(objectToQueryString(fullArgs)).replace('/', '_') + '.' + imageExt,
+        var encodedCall = base64Encode(objectToQueryString(fullArgs)).replace('/', '_') + '.' + imageExt,
             resizedImageUrl = configService.get('imageResizerEndpoint') + encodedCall;
 
         resizeUrlCache[cacheKey] = resizedImageUrl;
